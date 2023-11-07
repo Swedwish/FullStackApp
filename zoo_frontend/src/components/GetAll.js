0f28da0b00c9223a114623c798f8e573a255d236
Swedwish <vladsaburov99@gmail.com>
2023-11-07 18:48:51 +0700

    Optimised a little.

--- zoo_frontend/src/components/GetAll.js
+++ zoo_frontend/src/components/GetAll.js
@@ -8,6 +8,7 @@ export default function GetAllRows(props) {
     console.log(className);
     const [data, setData] = useState(null);
     const paperStyle = { padding: '50px 20px', width: 600, margin: "20px auto" };
+
     const handleGetAll = () => {
         console.log(className);
         fetch(`http://localhost:8080/${className}/findAll`)
@@ -16,6 +17,80 @@ export default function GetAllRows(props) {
                 setData(result);
             });
     };
+
+    const renderDataByClass = (item) => {
+        switch (className) {
+            case "animal":
+                return (
+                    <Paper elevation={6} style={{ margin: "10px", padding: "15px", textAlign: "left" }} key={item.id}>
+                        {item.id ? `Id: ${item.id} |` : ""}
+                        {item.name ? `Name: ${item.name} |` : ""}
+                        {item.species ? `Species: ${item.species} |` : ""}
+                        {item.dateOfBirth ? `Date of birth: ${formatDate(item.dateOfBirth)} |` : ""}
+                        {item.gender ? `Gender: ${item.gender} |` : ""}
+                        {item.cell && item.cell.id ? `Cell id: ${item.cell.id} |` : ""}
+                    </Paper>
+                );
+
+            case "cell":
+                return (
+                    <Paper elevation={6} style={{ margin: "10px", padding: "15px", textAlign: "left" }} key={item.id}>
+                        {item.id ? `ID: ${item.id} |` : ""}
+                        {item.sizeM3 ? `Size (m^3): ${item.sizeM3} |` : ""}
+                        {item.averageTemperature ? `Average Temperature: ${item.averageTemperature} |` : ""}
+                    </Paper>
+                );
+            case "diet":
+                return (
+                    <Paper elevation={6} style={{ margin: "10px", padding: "15px", textAlign: "left" }} key={item.id}>
+                        {item.id ? `ID: ${item.id} |` : ""}
+                        {item.animal ? `Animal ID: ${item.animal.id} |` : ""}
+                        {item.food ? `Food Name: ${item.food.name} |` : ""}
+                        {item.amountKg ? `Amount (kg): ${item.amountKg} |` : ""}
+                    </Paper>
+                );
+            case "food":
+                return (
+                    <Paper elevation={6} style={{ margin: "10px", padding: "15px", textAlign: "left" }}>
+                        {item.name ? `Name: ${item.name} |` : ""}
+                        {item.kcalories ? `Calories (kcal): ${item.kcalories} |` : ""}
+                        {item.isVegetarian !== undefined ? `Is Vegetarian: ${item.isVegetarian ? "Yes" : "No"} |` : ""}
+                    </Paper>
+                );
+            case "foodRetailer":
+                return (
+                    <Paper elevation={6} style={{ margin: "10px", padding: "15px", textAlign: "left" }}>
+                        {item.id ? `ID: ${item.id}|` : ""}
+                        {item.name ? `Company Name: ${item.name} |` : ""}
+                        {item.food ? `Food Name: ${item.food.name} |` : ""}
+                        {item.price ? `Price: ${item.price} |` : ""}
+                    </Paper>
+                );
+            case "job":
+                return (
+                    <Paper elevation={6} style={{ margin: "10px", padding: "15px", textAlign: "left" }}>
+                        {item.id ? `Id: ${item.id} |` : ""}
+                        {item.worker ? `Worker ID: ${item.worker.id} |` : ""}
+                        {item.animal ? `Animal ID: ${item.animal.id} |` : ""}
+                        {item.jobDescription ? `Job Description: ${item.jobDescription} |` : ""}
+                    </Paper>
+                );
+            case "worker":
+                return (
+                    <Paper elevation={6} style={{ margin: "10px", padding: "15px", textAlign: "left" }}>
+                        {item.id ? `Id: ${item.id} |` : ""}
+                        {item.name ? `Name: ${item.name} |` : ""}
+                        {item.dateOfBirth ? `Date of Birth: ${item.dateOfBirth} |` : ""}
+                        {item.salary ? `Salary: ${item.salary} |` : ""}
+                        {item.jobTitle ? `Job Title: ${item.jobTitle} |` : ""}
+                        {item.gender ? `Gender: ${item.gender} |` : ""}
+                    </Paper>
+                );
+            default:
+                return null;
+        }
+    };
+
     function formatDate(date) {
         if (!date) {
             return "";
@@ -24,8 +99,6 @@ export default function GetAllRows(props) {
         const formattedDate = new Date(date).toLocaleDateString(); // Format as "MM/DD/YYYY"
         return formattedDate;
     }
-
-
 
     return (
         <Container>
@@ -47,62 +120,11 @@ export default function GetAllRows(props) {
                 </Box>
             </Paper>
 
-            {data && <Paper elevation={3} style={paperStyle} >
-                {data.map((item) => (
-                    (className === "animal") ? (
-                        <Paper elevation={6} style={{ margin: "10px", padding: "15px", textAlign: "left" }} key={item.id}>
-                            {item.id ? `Id: ${item.id} |` : ""}
-                            {item.name ? `Name: ${item.name} |` : ""}
-                            {item.species ? `Species: ${item.species} |` : ""}
-                            {item.dateOfBirth ? `Date of birth: ${formatDate(item.dateOfBirth)} |` : ""}
-                            {item.gender ? `Gender: ${item.gender} |` : ""}
-                            {item.cell && item.cell.id ? `Cell id: ${item.cell.id} |` : ""}
-                        </Paper>
-                    ) : (className === "cell") ? (
-                        <Paper elevation={6} style={{ margin: "10px", padding: "15px", textAlign: "left" }} key={item.id}>
-                            {item.id ? `ID: ${item.id} |` : ""}
-                            {item.sizeM3 ? `Size (m^3): ${item.sizeM3} |` : ""}
-                            {item.averageTemperature ? `Average Temperature: ${item.averageTemperature} |` : ""}
-                        </Paper>
-                    ) : (className === "diet") ? (
-                        <Paper elevation={6} style={{ margin: "10px", padding: "15px", textAlign: "left" }} key={item.id}>
-                            {item.id ? `ID: ${item.id} |` : ""}
-                            {item.animal ? `Animal ID: ${item.animal.id} |` : ""}
-                            {item.food ? `Food Name: ${item.food.name} |` : ""}
-                            {item.amountKg ? `Amount (kg): ${item.amountKg} |` : ""}
-                        </Paper>
-                    ) : (className === "food") ? (
-                        <Paper elevation={6} style={{ margin: "10px", padding: "15px", textAlign: "left" }}>
-                            {item.name ? `Name: ${item.name} |` : ""}
-                            {item.kcalories ? `Calories (kcal): ${item.kcalories} |` : ""}
-                            {item.isVegetarian !== undefined ? `Is Vegetarian: ${item.isVegetarian ? "Yes" : "No"} |` : ""}
-                        </Paper>
-                    ) : (className === "foodRetailer") ? (
-                        <Paper elevation={6} style={{ margin: "10px", padding: "15px", textAlign: "left" }}>
-                            {item.id ? `ID: ${item.id}|` : ""}
-                            {item.name ? `Company Name: ${item.name} |` : ""}
-                            {item.food ? `Food Name: ${item.food.name} |` : ""}
-                            {item.price ? `Price: ${item.price} |` : ""}
-                        </Paper>
-                    ) : (className === "job") ? (
-                        <Paper elevation={6} style={{ margin: "10px", padding: "15px", textAlign: "left" }}>
-                            {item.id ? `Id: ${item.id} |` : ""}
-                            {item.worker ? `Worker ID: ${item.worker.id} |` : ""}
-                            {item.animal ? `Animal ID: ${item.animal.id} |` : ""}
-                            {item.jobDescription ? `Job Description: ${item.jobDescription} |` : ""}
-                        </Paper>
-                    ) : (className === "worker") ? (
-                        <Paper elevation={6} style={{ margin: "10px", padding: "15px", textAlign: "left" }}>
-                            {item.id ? `Id: ${item.id} |` : ""}
-                            {item.name ? `Name: ${item.name} |` : ""}
-                            {item.dateOfBirth ? `Date of Birth: ${item.dateOfBirth} |` : ""}
-                            {item.salary ? `Salary: ${item.salary} |` : ""}
-                            {item.jobTitle ? `Job Title: ${item.jobTitle} |` : ""}
-                            {item.gender ? `Gender: ${item.gender} |` : ""}
-                        </Paper>
-                    ) : null
-                ))
-                }</Paper>}
+            {data && (
+                <Paper elevation={3} style={paperStyle}>
+                    {data.map((item) => renderDataByClass(item))}
+                </Paper>
+            )}
         </Container>
     );
 }
